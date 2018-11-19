b90baf78a2505b532a0c04f73cbacbf87f8b1a6e
bolften <carlos.axer96@gmail.com>
2018-11-19 20:38:04 +0000

    Practica finalizada - cumplidas las pruebas de la clasificacion de icm

--- lib/Individuo.rb
+++ lib/Individuo.rb
@@ -42,7 +42,19 @@ class Pacientes < Individuo
     end
     
     def indice_corporal
-        
+        if @datos.indice_masa_corporal <= 18.5
+            "Bajo peso"
+        elsif @datos.indice_masa_corporal > 18.5 and @datos.indice_masa_corporal <= 24.9
+            "Peso adecuado"
+        elsif @datos.indice_masa_corporal > 25.0 and @datos.indice_masa_corporal <= 29.9
+            "Sobrepeso"
+        elsif @datos.indice_masa_corporal > 30.0 and @datos.indice_masa_corporal <= 34.9
+            "Obesidad grado 1"
+        elsif @datos.indice_masa_corporal > 35.0 and @datos.indice_masa_corporal <= 39.9
+            "Obesidad grado 2"
+        elsif @datos.indice_masa_corporal > 40
+            "Obesidad grado 3"
+        end
     end
     
     def to_s
